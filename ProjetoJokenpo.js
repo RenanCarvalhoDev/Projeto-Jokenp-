f03d0884d0b5e7489b7f4edcb3d8c3e0bb01826d
RenanCarvalhoDev <renancarvalho.dev@outlook.com>
2022-03-14 22:36:51 -0300

    Commit de atualização de funcionalidade V.002

--- ProjetoJokenpo.js
+++ ProjetoJokenpo.js
@@ -1,8 +1,8 @@
 //Projeto 2 (Jokenpô) - Renan Augusto de Carvalho
 
-
 console.log("Olá, aqui é o jogo de jokenpô, abaixo, escolha quantas vezes deseja jogar e qual o sua ecolha (PEDRA, PAPEL ou TESOURA)");
-let round = +prompt("Quantas rodadas será o jogo ??");
+
+let round = +prompt("Quantas rodadas será o jogo ?? ");
 let a = 0;
 let b = 0;
 let c = 0;
@@ -20,7 +20,7 @@ while( c  < round ){
 
     if (option[a]>= 1 && option[a] <=3){
         
-        randon[b] = parseInt(Math.floor(Math.random() * 3 +1 ));
+        randon[b] = parseInt(Math.random() * 3 +1 );
         
         if ((randon[b] == 3 && option[a] == 1)||(randon[b] == 1 && option[a] == 2)||(randon[b] == 2 && option[a] == 3)){
         
@@ -36,7 +36,7 @@ while( c  < round ){
             console.log("\nJooookeeeennnpoooô!");
             console.log(`\nComputador : ${randon[b]} --- Jogador : ${option[a]}`);
             console.log("O COMPUTADOR ganhou essa !!!");
-            
+           
             winnerComputer ++;   
         }
 
@@ -45,11 +45,9 @@ while( c  < round ){
             console.log("\nJooookeeeennnpoooô!");
             console.log(`\nComputador : ${randon[b]} --- Jogador : ${option[a]}`);
             console.log("Aconteceu o inexplicavel, houve um EMPATE !!!");
-           
+            
             tie ++;   
         }
-        
-        
 
         b++;
         a++;
@@ -58,10 +56,8 @@ while( c  < round ){
 }
 
 
+prompt("\nAperte ENTER para ver o resultado");
 console.clear();
 console.log(`O JOGADOR teve ${winnerPlayer} vitoria(s)\n`);
 console.log(`O COMPUTADOR teve ${winnerComputer} vitoria(s)\n`);
 console.log(`Teve ${tie} empate(s)\n`);
-
-
-    
