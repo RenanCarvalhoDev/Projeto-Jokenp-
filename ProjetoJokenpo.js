a1ccc5d14aead57da2893bb58822af8318049b27
RenanCarvalhoDev <renancarvalho.dev@outlook.com>
2022-03-17 23:03:30 -0300

    Alteração de cores mostradas e de seleçaõ de array

--- ProjetoJokenpo.js
+++ ProjetoJokenpo.js
@@ -8,45 +8,86 @@ let tie  = 0;
 let option = [];
 let randon = [];
 let answer;
+let a,b,c;
+let computer, options ;
+
+function nameComputer (){
+  switch (randon [b]){
+        case 1:
+          computer = "\033[1;31mPEDRA\033[0;0m"
+        break;
+
+        case 2:
+          computer = "\033[1;31mPAPEL\033[0;0m"
+        break;
+        
+        case 3:
+          computer = "\033[1;31mTESOURA\033[0;0m"
+         break; 
+  }
+}
+
+function colorOption (){
+  switch (option [a]){
+        case 'PEDRA':
+          options = "\033[1;31mPEDRA\033[0;0m"
+        break;
+
+        case 'PAPEL':
+          options = "\033[1;31mPAPEL\033[0;0m"
+        break;
+        
+        case 'TESOURA':
+          options = "\033[1;31mTESOURA\033[0;0m"
+         break; 
+  }
+}
 
 do {      
           let round = +prompt("\nQuantas rodadas será o jogo ?? ");
-          let a = 0;
-          let b = 0;
-          let c = 0;
+          a = 0;
+          b = 0;
+          c = 0;
           console.clear();
-          while( c  < round ){
+
+          for( c = 0; c < round ; c++){
                 
             console.log("\nEscolha uma opção:\n");
-            console.log("    1 : PEDRA ---  2 : PAPEL  ---  3 : TESOURA\n");
-            option [a] = option.push = +prompt(": ");
+            console.log("   PEDRA   ---    PAPEL  ---  TESOURA\n");
+            option [a] = prompt(": ").toUpperCase();
 
-            if (option[a]>= 1 && option[a] <=3){
+            if (option[a] == "PEDRA" || option[a] == "PAPEL" || option[a] == "TESOURA"){
                     
-              randon[b] = parseInt(Math.random() * 2 +1 );
+              randon[b] = Number(parseInt(Math.random() * 3 +1 ));
                     
-                  if ((randon[b] == 3 && option[a] == 1)||(randon[b] == 1 && option[a] == 2)||(randon[b] == 2 && option[a] == 3)){
+                  if ((randon[b] == 3 && option[a] == 'PEDRA')||(randon[b] == 1 && option[a] == "PAPEL")||(randon[b] == 2 && option[a] == "TESOURA")){
                     
+                    nameComputer();
+                    colorOption ();
                     console.log("\nJooookeeeennnpoooô!");
-                    console.log(`\nComputador : ${randon[b]} --- Jogador : ${option[a]}`);
+                    console.log(`\nComputador :  ${computer}  <---> Jogador : ${options}`);
                     console.log("O JOGADOR ganhou essa !!!");
                         
                     winnerPlayer ++;   
                     }
 
-                  if ((randon[b] == 1 && option[a] == 3)||(randon[b] == 2 && option[a] == 1)||(randon[b] == 3 && option[a] == 2)){
+                  if ((randon[b] == 1 && option[a] == "TESOURA")||(randon[b] == 2 && option[a] == "PEDRA")||(randon[b] == 3 && option[a] == "PAPEL")){
                     
+                    nameComputer();
+                    colorOption ();
                     console.log("\nJooookeeeennnpoooô!");
-                    console.log(`\nComputador : ${randon[b]} --- Jogador : ${option[a]}`);
+                    console.log(`\nComputador : ${computer} <---> Jogador : ${options}`);
                     console.log("O COMPUTADOR ganhou essa !!!");
                     
                     winnerComputer ++;   
                     }
 
-                  if ((randon[b] == 1 && option[a] == 1)||(randon[b] == 2 && option[a] == 2)||(randon[b] == 3 && option[a] == 3)){
+                  if ((randon[b] == 1 && option[a] == "PEDRA")||(randon[b] == 2 && option[a] == "PAPEL")||(randon[b] == 3 && option[a] == "TESOURA")){
                     
+                    nameComputer();
+                    colorOption ();
                     console.log("\nJooookeeeennnpoooô!");
-                    console.log(`\nComputador : ${randon[b]} --- Jogador : ${option[a]}`);
+                    console.log(`\nComputador : ${computer} <---> Jogador : ${options}`);
                     console.log("Aconteceu o inexplicavel, houve um EMPATE !!!");
                         
                     tie ++;   
@@ -54,15 +95,33 @@ do {
 
                     b++;
                     a++;
-                    c++;
+                    
               }
           }
 
           answer = prompt("\nDeseja jogar outra vez ?").toLowerCase();
+
     }while(answer == 'sim'|| answer == 's')
 
-            prompt("\nAperte ENTER para ver o resultado");
-            console.clear();
-            console.log(`O JOGADOR teve ${winnerPlayer} vitoria(s)\n`);
-            console.log(`O COMPUTADOR teve ${winnerComputer} vitoria(s)\n`);
-            console.log(`Teve ${tie} empate(s)\n`);
+prompt("\nAperte ENTER para ver o resultado");
+console.clear();
+console.log(`O JOGADOR teve ${winnerPlayer} vitoria(s)\n`);
+console.log(`O COMPUTADOR teve ${winnerComputer} vitoria(s)\n`);
+console.log(`Teve ${tie} empate(s)\n`);
+
+if (winnerPlayer > winnerComputer && winnerPlayer > tie){
+
+    console.log("O Jogador é o GRANDE VENCEDOR !!!! ");
+  
+}else if (winnerComputer > winnerPlayer && winnerComputer > tie){
+
+    console.log("O Computador é o GRANDE VENCEDOR !!!! ");
+}
+
+else{
+
+  console.log("Acontenceu o inesplicavel é EMPATE !!!! ");
+}
+
+
+            
