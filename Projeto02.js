67b3ef951c52feeaa95d241a56f3cf9f87469d43
RenanCarvalhoDev <renancarvalho.dev@outlook.com>
2022-03-21 17:11:57 -0300

    Adicionando comentarios  e mudando o titulo

--- Projeto02.js
+++ Projeto02.js
@@ -1,7 +1,8 @@
 //Projeto 2 (Jokenpô) - Renan Augusto de Carvalho
 
-console.log("Olá, você está no jogo de \033[1;33mJOKENPÔ\033[0;0m , abaixo, escolha quantas vezes deseja jogar : \n");
+console.log("Olá, você está no jogo de \033[1;33mJOKENPÔ\033[0;0m , abaixo, escolha quantas vezes deseja jogar : \n");//Mostrando a primeira informação do jogo e mudando a colocação da letra JOKENPO.
 
+//declarando as variveis comuns e as array.
 let winnerPlayer = 0;
 let winnerComputer = 0;
 let tie  = 0;
@@ -11,6 +12,7 @@ let answer;
 let a,b,c;
 let computer;
 
+//Criando uma função para gravar numa variavel qual o valor de Math.randon será relacionado a uma string do jogo.
 function nameComputer (){
   switch (randon [b]){
         case 1:
@@ -27,26 +29,31 @@ function nameComputer (){
   }
 }
 
+
+//Aqui é onde a Magica acontece: Aqui esta todo o programa num loop finito, onde exite ele para conseguir realizar o jogo quantas vezes o usuario quiser.
 do {      
+          
+          // Realizando uma impressão de uma pergunta para o usuario e recebendo essa informação para dentro de uma variavel e após isso declarando mais algumas variaveis.
           let round = +prompt("Quantas rodadas será o jogo ?? ");
           a = 0;
           b = 0;
           c = 0;
           console.clear();
 
+          //realizando um loop finito para fazer toda a dinamica do jogo de pergunta de quais opções o usuario irá querer, quanto gerar valores aleatorios pelo Math.randon.
           for( c = 0; c < round ; c++){
                 
             console.log("\nEscolha uma opção:\n");
             console.log("   \033[0;33mPEDRA\033[0;0m   ---    PAPEL  ---  \033[1;90mTESOURA\033[0;0m\n");
             option [a] = prompt(": ").toUpperCase();
 
-            if (option[a] == "PEDRA" || option[a] == "PAPEL" || option[a] == "TESOURA"){
+            if (option[a] == "PEDRA" || option[a] == "PAPEL" || option[a] == "TESOURA"){//verificando o que o usuario digitou se é pedra, papel ou tesoura.
                     
-              randon[b] = Number(parseInt(Math.random() * 3 +1 ));
+              randon[b] = Number(parseInt(Math.random() * 3 +1 ));//Gerando valores aleatorios entre 1 e 3, transformando em numero e adicionando a um array.
                     
-                  if ((randon[b] == 3 && option[a] == 'PEDRA')||(randon[b] == 1 && option[a] == "PAPEL")||(randon[b] == 2 && option[a] == "TESOURA")){
+                  if ((randon[b] == 3 && option[a] == 'PEDRA')||(randon[b] == 1 && option[a] == "PAPEL")||(randon[b] == 2 && option[a] == "TESOURA")){//comparando a opção que o usuario escolheu e o numero aleatorio que foi gerado pelo Math.randon.
                     
-                    nameComputer();                    
+                    nameComputer();//Chamando a função para trocar o valor do Math.randon de numero par uma palavra.                    
                     console.log("\nJooookeeeennnpoooô!");
                     console.log("\nComputador: \033[1;31m"+ computer + "\033[0;0m <---> Jogador : \033[1;32m"+ option[a] + "\033[0;0m" );
                     console.log("O JOGADOR ganhou essa !!!\n");                       
@@ -54,9 +61,9 @@ do {
                     
                     }
 
-                  if ((randon[b] == 1 && option[a] == "TESOURA")||(randon[b] == 2 && option[a] == "PEDRA")||(randon[b] == 3 && option[a] == "PAPEL")){
+                  if ((randon[b] == 1 && option[a] == "TESOURA")||(randon[b] == 2 && option[a] == "PEDRA")||(randon[b] == 3 && option[a] == "PAPEL")){//comparando a opção que o usuario escolheu e o numero aleatorio que foi gerado pelo Math.randon.
                     
-                    nameComputer();
+                    nameComputer();//Chamando a função para trocar o valor do Math.randon de numero par uma palavra.
                     console.log("\nJooookeeeennnpoooô!");
                     console.log("\nComputador: \033[1;32m"+ computer +"\033[0;0m <---> Jogador : \033[1;31m"+ option[a] +"\033[0;0m" );
                     console.log("O COMPUTADOR ganhou essa !!!\n");
@@ -64,9 +71,9 @@ do {
                     
                     }
 
-                  if ((randon[b] == 1 && option[a] == "PEDRA")||(randon[b] == 2 && option[a] == "PAPEL")||(randon[b] == 3 && option[a] == "TESOURA")){
+                  if ((randon[b] == 1 && option[a] == "PEDRA")||(randon[b] == 2 && option[a] == "PAPEL")||(randon[b] == 3 && option[a] == "TESOURA")){//comparando a opção que o usuario escolheu e o numero aleatorio que foi gerado pelo Math.randon.
                     
-                    nameComputer();
+                    nameComputer();//Chamando a função para trocar o valor do Math.randon de numero par uma palavra.
                     console.log("\nJooookeeeennnpoooô!");
                     console.log("\nComputador: \033[1;34m"+ computer +"\033[0;0m <---> Jogador : \033[1;34m"+ option[a] +"\033[0;0m" );
                     console.log("Aconteceu o inexplicavel, houve um EMPATE !!!\n");   
@@ -80,16 +87,18 @@ do {
               }
           }
 
-          answer = prompt("Deseja jogar outra vez ?").toLowerCase();
+          answer = prompt("Deseja jogar outra vez ?").toLowerCase();// realizando a pergunta para saber se o usuario deseja jogar novamente, recebendo o valor em letra minuscula e adicionando na variavel.
 
     }while(answer == 'sim'|| answer == 's')
 
+//mostrando o resultado de todas as jogadas, quantidade de vitorias entre usuario e computador, ou empates.
 prompt("Aperte ENTER para ver o resultado");
 console.clear();
 console.log(`O JOGADOR teve ${winnerPlayer} vitoria(s)\n`);
 console.log(`O COMPUTADOR teve ${winnerComputer} vitoria(s)\n`);
 console.log(`Teve ${tie} empate(s)\n`);
 
+//Verificando quem foi o jogador de todas as rodas.
 if (winnerPlayer > winnerComputer){
 
     console.log("O \033[1;93mJogador\033[0;0m é o \033[1;93mGRANDE VENCEDOR !!!!\033[0;0m ");
@@ -105,4 +114,4 @@ else{
 }
 
 
-            
+//FIM DO CÓDIGO.
